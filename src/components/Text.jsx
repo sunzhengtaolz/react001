5cb3071e3cdfab40bf7b41b136f9140085647063
sunzhengtao <sunzhengtaolz@qq.com>
2018-10-06 03:25:10 +0800

    暴露webpack,安装less/less-loader依赖

---
+++ src/components/Text.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './Text.less';
+
+class Text extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="textMain">
+
+            </div>
+         );
+    }
+}
+ 
+export default Text;
